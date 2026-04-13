307c1ea51796a2b0c51566eeeacc1f99ab0dda40
DanielNkunzimana <mcdanielgolden11@gmail.com>
2026-04-13 20:28:33 +0200

    added readme file

--- obj/Debug/net9.0/Adventure_game.AssemblyInfo.cs
+++ obj/Debug/net9.0/Adventure_game.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Adventure_game")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea6557e810597b95b773e47f15e6f25da27ced00")]
 [assembly: System.Reflection.AssemblyProductAttribute("Adventure_game")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Adventure_game")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
